8a5506146bb672a5555bbd25989c0ae37487cd9c
Aayush Chimaniya <chimaniyaaayush04@gmail.com>
2023-12-15 11:00:06 +0530

    intial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+    document.getElementById("loadBtn").addEventListener("click",()=>{
+        document.getElementById("loaderScreen").style.display="block";
+    })
+
+    document.getElementById("loaderScreen").addEventListener("click",()=>{
+        document.getElementById("loaderScreen").style.display="none";
+    })
+
+
