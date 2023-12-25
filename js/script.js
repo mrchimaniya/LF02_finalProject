61f7ae76a5d52bd928cba0c4a4a14ea9964a77bc
Aayush Chimaniya <chimaniyaaayush04@gmail.com>
2023-12-25 08:12:15 +0000

    responsive all website but nav bar not done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+initLocomotiveWithScrollTrigger=function(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+}
+
+initLocomotiveWithScrollTrigger();
+
+
 gsap.from("#nav",{
     y:-100,
     delay:0.5,
@@ -31,7 +65,7 @@ gsap.from("#page1>h1",{
 var tl=gsap.timeline({
     scrollTrigger:{
         trigger:"#page1",
-        scroller:"body",
+        scroller:"#main",
         // markers:true,
         start:"top -5%",
         scrub:4,
@@ -41,11 +75,11 @@ var tl=gsap.timeline({
 var t2=gsap.timeline({
     scrollTrigger:{
         trigger:"#page1",
-        scroller:"body",
-        markers:true,
-        start:"top -560%",
+        scroller:"#main",
+        // markers:true,
+        start:"top -540%",
         end:"top -620%",
-        scrub:2,
+        scrub:1,
     }
 });
 
@@ -118,3 +152,9 @@ floatingImagesDiv.forEach((elem)=>{
         // elem.childNodes[7].style.top=`${dets.y}px`;
     });
 })
+
+// gsap.to("#navPart1navInner h3",{
+//     y:"-210%",
+//     delay:2,
+//     duration:2,
+// });
